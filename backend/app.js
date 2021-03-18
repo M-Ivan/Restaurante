0ea0edb35f99d4e4a2733c1efd8a48c22a5591da
Ivan Miragaya <MiragayaIvan@hotmail.com>
2021-03-18 14:08:45 -0300

    heroku

--- backend/app.js
+++ backend/app.js
@@ -90,6 +90,7 @@ app.use(function (err, req, res, next) {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
